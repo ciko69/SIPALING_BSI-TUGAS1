f97acee913b3066f8cc13064870679b77c9a6283
Ciko69 <noufalburhan@outlook.com>
2025-04-28 00:19:29 +0700

    profil.blade.php

---
+++ profil.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MY PROFIL</title>
+
+    <style>
+        body {
+            font-family: "Segoe UI", sans-serif;
+            background-image: url(https://lh3.googleusercontent.com/pw/AP1GczOyJbbrOQmAYqS7EAiJeVY6pMvrjs2h-K7kBZr-z9E0ny-loWQDEPy_BwFVM92fPeOtlBCNP-VFy1wCx_5Jd3s_PqSxyqevBmkfs9eePJtIJHmfTV7gKJuKOYQT3CnU4bLS9KHEcR2iwVJjU_l4eBDz=w1718-h1145-s-no-gm);
+            background-repeat: no-repeat;
+            width: 100%;
+            height: 100vh;
+            background-size: cover;
+        }
+
+        .profile-card {
+            max-width: 400px;
+            margin: 50px auto;
+            background: white;
+            padding: 30px;
+            border-radius: 20px;
+            box-shadow: 0 10px 30px rgba(0,0,0,0.1);
+            text-align: center;
+        }
+
+        .profile-card img {
+            width: 120px;
+            height: 120px;
+            object-fit: cover;
+            border-radius: 50%;
+            border: 4px solid #ff9a9e;
+            margin-bottom: 20px;
+        }
+
+        .profile-card h2 {
+            margin: 0;
+            font-size: 24px;
+            color: #764ba2;
+        }
+
+        .profile-card p {
+            font-size: 16px;
+            color: #666;
+        }
+    </style>
+</head>
+    <body>
+        <div class="profile-card">
+        <img src="https://lh3.googleusercontent.com/pw/AP1GczMWhZiiU10JbWE7J5HqURnzjXGgg9m7Bl5tQ60Ca9eVQpiCHTfR0VnnNHhmS_40eEkQd_8v6ahSXDe3o9jWAB9X_9Pcl6-4rPbzOqqi7YO35Ve2Itk=w960-h1047-no" alt="Foto Profil">
+        <h2>Naufal Burhanuddin Yusuf</h2>
+        <p>Cyber Security &amp; Backend Development.</p>
+        <p>NIM : 17231129</p>
+        <p>KELAS : 17.4B.05</p>
+        </div>
+    </body>
+</html>
